e75a6032de5c5663aa64e1a24059314ed9864cc7
Whilmar Bitoco <whlmrbitoco@gmail.com>
2025-10-06 22:46:18 +0800

    changed readme

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,5 @@
+const SignIn = () => {
+  return <div>page</div>;
+};
+
+export default SignIn;
---
+++ src/lib/firebase.client.ts
@@ -0,0 +1,20 @@
+// lib/firebase.js
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MSG_ID || "",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
