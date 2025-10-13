0ce3ea92982030c503ce6f12720339cb590c4f0b
Whilmar Bitoco <whlmrbitoco@gmail.com>
2025-10-13 14:50:51 +0800

    add api endpoint

---
+++ src/app/api/weather/route.ts
@@ -0,0 +1,25 @@
+import { isParementersMissing } from "@/lib/utils";
+import { Weather } from "@/types";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const url = new URL(request.url);
+    const deviceId = url.searchParams.get("deviceId");
+    const { weather }: { weather: Weather } = await request.json();
+
+    if (isParementersMissing([deviceId, weather])) {
+      return NextResponse.json(
+        { error: "Missing parameters" },
+        { status: 400 }
+      );
+    }
+
+    NextResponse.json({ deviceId });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to process request" },
+      { status: 500 }
+    );
+  }
+}
